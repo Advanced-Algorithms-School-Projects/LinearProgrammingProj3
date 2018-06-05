b3a33ef758745b69348cbf75884f1041ae46cc93
Patrick Hanbury <patrickhanbury1@gmail.com>
2018-06-05 18:48:49 -0400

    fixed ./ problem

--- knapsack.sh
+++ knapsack.sh
@@ -3,7 +3,7 @@
 datanames='knapsack8 knapsack12 knapsack16 knapsack20 knapsack28 knapsack32 knapsack48 knapsack64 knapsack128 knapsack256 knapsack512 knapsack1024'
 datanamesarray=($datanames)
 for item in ${datanamesarray[@]}; do
-  ( ./ampl ;
+  ( ampl ;
 model knapsack.mod ; 
 data knapsack-dat/${item}.dat ; 
 solve;
